9179308014ec979f83c3c01161fa072b4a4fd384
Yash0162004 <104299492+Yash0162004@users.noreply.github.com>
2025-02-11 04:33:19 -0800

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
-  plugins: [reactRefresh()],
   resolve: {
     alias: {
       '@react-icons/bs': 'react-icons/bs',
