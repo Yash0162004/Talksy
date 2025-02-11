19cb3b32a2f4b99cd50e7831e8113973289206e6
Yash0162004 <104299492+Yash0162004@users.noreply.github.com>
2025-02-11 04:07:50 -0800

    ChatContainer.jsx

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -2,7 +2,7 @@ import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
-import MessageSkeleton from '../skeletons/MessageSkeleton';
+import MessageSkeleton from "../components/Skeletons/MessageSkeleton"
 import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/util";
 
