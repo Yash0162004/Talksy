ca54feb84816aa8361bd7d4e277d467ea7a9aaab
Yash0162004 <104299492+Yash0162004@users.noreply.github.com>
2025-02-11 06:16:36 -0800

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@react-icons/bs': 'react-icons/fa',
-    },
-  },
   build: {
     rollupOptions: {
-      external: ['react-icons'], // Add this line
+      external: ['react-icons/fa'], // Add this line
     },
   },
 });
