68717b2b35f0e1d59fbb94ee4d440446eedcdf62
Yash0162004 <104299492+Yash0162004@users.noreply.github.com>
2025-02-11 05:40:20 -0800

    Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
 import { LogOut, Settings, User } from "lucide-react";
-import { BsIconName } from '@react-icons/bs';
-
+import { FaIconName } from 'react-icons/fa';
 const Navbar = () => {
   const { logout, authUser  } = useAuthStore();
 
