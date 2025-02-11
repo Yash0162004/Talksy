0d837a54c3b54350e43351eb477bb8029cfa96b1
Yash0162004 <104299492+Yash0162004@users.noreply.github.com>
2025-02-11 04:12:00 -0800

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   // ... other options ...
   build: {
     rollupOptions: {
-      external: ['@react-icons/bs'],
+      external: ['react-icons/bs'],
     },
   },
 });
