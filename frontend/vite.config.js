fcb65c44c50fbfed350826599e9c522da02a7f7b
Yash0162004 <104299492+Yash0162004@users.noreply.github.com>
2025-02-11 08:23:43 -0800

    Updated vite.config.js to resolve react-icons import issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['react-icons/fa'], // Add this line
+    // external: ['react-icons/fa'], // Remove this line
+
     },
   },
 });
